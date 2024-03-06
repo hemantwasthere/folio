46bfd4c88a2046be948ecde7e37bcedc3190fe37
Hemant <hrsmart2150@gmail.com>
2024-03-06 18:33:40 +0530

    adding piano

---
+++ components/Synth/SynthBase.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+
+const SynthBase: React.FC = () => {
+  return (
+    <Fragment>
+      <g id="synth-base">
+        <rect
+          className="synth-base"
+          x="0"
+          y="0"
+          width="270"
+          height="156"
+          rx="12"
+        ></rect>
+      </g>
+    </Fragment>
+  );
+};
+
+export default SynthBase;
--- components/organisms/Hero.tsx
+++ components/organisms/Hero.tsx
@@ -3,36 +3,37 @@ import Link from "next/link";
 import Button from "@/components/atoms/Button";
 import HeroImage from "@/components/atoms/HeroImage";
 import Socials from "@/components/molecules/Socials";
+import Synth from "../Synth/Synth";
 
 const Hero: React.FC = () => {
   return (
-    <>
-      <section
-        id="home"
-        className="wrapper scroll-mt-[20rem] block sm:flex justify-between items-center mb-[2.75rem] md:mb-[5.75rem] mt-[4rem] sm:mt-[5rem] md:mt-[6.5rem] lg:mt-[7rem]"
-      >
-        <div>
-          <h1
-            className='before:content-["///"] before:h-[300px] before:text-[175px] before:font-[700] before:-z-10 before:select-none before:translate-x-[-95%] before:translate-y-0 webkit_text_stroke before:opacity-[0.25] before:tracking-[-.1em] before:absolute
+    <section
+      id="home"
+      className="wrapper scroll-mt-[20rem] block sm:flex justify-between items-center mb-[2.75rem] md:mb-[5.75rem] mt-[4rem] sm:mt-[5rem] md:mt-[6.5rem] lg:mt-[7rem]"
+    >
+      <div>
+        <h1
+          className='before:content-["///"] before:h-[300px] before:text-[175px] before:font-[700] before:-z-10 before:select-none before:translate-x-[-95%] before:translate-y-0 webkit_text_stroke before:opacity-[0.25] before:tracking-[-.1em] before:absolute
                     text-[65px] md:text-[96px] text-text_primary font-semibold'
-          >
-            Hemant
-          </h1>
-          <h4 className="mt-4 text-[22px] md:text-[26px]">
-            Innovative Frontend Wizard.
-            <br className="hidden md:block" /> Transforming Designs into
-            Reality.
-          </h4>
-          <div className="mt-4 mb-[1.7rem]">
-            <Socials />
-          </div>
-          <Link tabIndex={-1} className="outline-none" href="#work">
-            <Button>Discover my work ↓</Button>
-          </Link>
+        >
+          Hemant
+        </h1>
+        <h4 className="mt-4 text-[22px] md:text-[26px]">
+          Innovative Frontend Wizard.
+          <br className="hidden md:block" /> Transforming Designs into Reality.
+        </h4>
+        <div className="mt-4 mb-[1.7rem]">
+          <Socials />
         </div>
-        <HeroImage />
-      </section>
-    </>
+        <Link tabIndex={-1} className="outline-none" href="#work">
+          <Button>Discover my work ↓</Button>
+        </Link>
+      </div>
+      {/* <HeroImage /> */}
+      <div className="synth">
+        <Synth />
+      </div>
+    </section>
   );
 };
 
