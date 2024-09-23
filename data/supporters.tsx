e448c88d225c3aa5a63be8c62abcd8f9bc7db952
Hemant <hrsmart2150@gmail.com>
2024-09-23 20:13:46 +0530

    chore: updated redirection of supporters

--- data/supporters.tsx
+++ data/supporters.tsx
@@ -1,22 +1,21 @@
 import { useTranslations } from "next-intl";
 
 export function getSupportersData() {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const t = useTranslations("Supporters");
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const t = useTranslations("Supporters");
 
-    return [
-        {
-            "name": t("supporter1.name"),
-            "icon": "/supporters/piyush.png",
-            "href": "https://www.youtube.com/@piyushgargdev",
-            "message": t("supporter1.message")
-        },
-        {
-            "name": t("supporter2.name"),
-            "icon": "/supporters/vedant.jpeg",
-            "href": "https://www.youtube.com/@js_cafe",
-            "message": t("supporter2.message")
-        }
-    ]
+  return [
+    {
+      name: t("supporter1.name"),
+      icon: "/supporters/piyush.png",
+      href: "https://x.com/piyushgarg_dev",
+      message: t("supporter1.message"),
+    },
+    {
+      name: t("supporter2.name"),
+      icon: "/supporters/vedant.jpeg",
+      href: "https://x.com/_vedantjain",
+      message: t("supporter2.message"),
+    },
+  ];
 }
-
